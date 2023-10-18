2a9fc499d13c8359ca34d37a85cb6a3028bca1e7
suddii01 <suddii01@naver.com>
2023-10-18 19:16:31 +0900

    feat: 다리 생성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,13 +1,17 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
         outputView.printStartMessage();
         int bridgeSize = inputView.readBridgeSize();
-
+        List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
     }
 }
