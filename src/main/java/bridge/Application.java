509b020de31cfa26ea5dd2e19a5e7ce3d7fe41d9
suddii01 <suddii01@naver.com>
2023-10-18 19:08:55 +0900

    feat: 다리 길이 입력

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,7 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
+        InputView inputView = new InputView();
         OutputView outputView = new OutputView();
+
         outputView.printStartMessage();
+        int bridgeSize = inputView.readBridgeSize();
+
     }
 }
